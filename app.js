f21defa569e828eb60cb592397d364501526700d
Diastowo <diastowo@gmail.com>
2020-05-15 14:03:30 +0700

    still logging

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // });
 
 const ACCESS_TOKEN = "";
-console.log(process.env.HEROKU_APP_NAME);
+console.log(process.env);
 var chatQue = new Queue('chatque', process.env.REDIS_URL);
 
 const CHAT_API_URL = "https://chat-api.zopim.com/graphql/request";
